b7994a8a0a80aa9207890b565ff60144bdf72035
anki <anki.makeit@gmail.com>
2017-12-16 02:11:48 +0530

    Setup start script and heroku port

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const hbs = require('hbs');
+const hbs = require('hbs')
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
+
 app.set('view engine','hbs');
 
-console.log("Directory name :" +process.cwd());
 app.use(express.static(process.cwd() +'/public'));
+app.use(bodyParser.json());
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+// for parsing multipart/form-data
+app.use(upload.array());
+
+
 app.use((req, res, next) =>{
    var now = new Date().toDateString();
    var log = `Time: ${now} Method : ${req.method} url: ${req.url}`;
@@ -39,5 +52,15 @@ app.get('/about',(req, res)=>{0
 });
 });
 
-app.listen(5000);
-console.log('Server listening on port 5000');
+// sending form to client
+app.get('/form', (req, res) =>{
+    res.render('form.hbs');
+});
+
+app.post('/', function(req, res) {
+
+    console.log(req.body);
+    res.send("recieved your request!");
+});
+app.listen(port);
+console.log('Server listening on port', port);
